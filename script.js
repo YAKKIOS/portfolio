2fd7c6eebf894f5b9bc4442a2986a748941eca09
YAKKIOS <49037960+YAKKIOS@users.noreply.github.com>
2026-05-22 14:46:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -190,8 +190,8 @@ function init() {
         "My dad was the mayor of Hawkinge from 2023-2024, which is a small town in Kent 🤴🏻",
         "I used to work as a car mechanic for fun whilst I studied at college 🔧",
         "I was featured in Sketch's best designs of 2022 ✏️",
-        "I was an avid horse rider until my sister was thrown from her horse and broke her shoulder... I promptly gave up the sport 🐎",
-        "I setup an Onlyfans page for margaritas called Onlymargs but has since been deprecated 🍸",
+        "I was an avid horse rider until my sister was thrown from her horse and shattered her shoulder and then I promptly gave up the sport 🐎",
+        "I started an Onlyfans page for margaritas called OnlyMargs but it has since been deprecated 🍸",
         "Whilst at Komodo Digital I accidentally had the office Christmas cards printed upside down and it's still talked about to this day 🎄",
         "I once drank an entire pitcher of raspberry mojito and I turned red like a scene from Charlie and the Chocolate Factory 😳",
         
